78f771f12d91b1cff85f1a0531ce3449081eb3e2
Nizami Tagiyev <nizamion24s@gmail.com>
2022-04-05 14:28:53 +0500

    LongPressGesture

---
+++ SwiftUI Bootcamp/LongPressGesture.swift
@@ -0,0 +1,64 @@
+//
+//  LongPressGesture.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Nizami Tagiyev on 05.04.2022.
+//
+
+import SwiftUI
+
+struct LongPressGesture: View {
+    
+    @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
+    
+    var body: some View {
+        VStack {
+            Rectangle()
+                .fill(isSuccess ? Color.green : Color.blue)
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            
+            HStack {
+                Text("Click here")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 2.0, maximumDistance: 50) {
+                        // At the min duration
+                        withAnimation(.easeInOut) {
+                            isSuccess = true
+                        }
+                    } onPressingChanged: { (isPressing) in
+                        // Start of press -> min duration
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 2.0)) {
+                                isComplete = true
+                            }
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess {
+                                    withAnimation(.easeInOut) {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                Text("Reset")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        isComplete = false
+                        isSuccess = false
+                    }
+            }
+        }
+    }
+}
