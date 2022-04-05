7c928fc15e6de115ddd7133e7d9b741f5bee4879
Nizami Tagiyev <nizamion24s@gmail.com>
2022-04-05 13:39:33 +0500

    Initial Commit

---
+++ SwiftUI Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Nizami Tagiyev on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
