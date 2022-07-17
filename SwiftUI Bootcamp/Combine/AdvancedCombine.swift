72a79b119577ef7a668d6820be75461ae9bf0ba9
Nizami Tagiyev <nizamion24s@gmail.com>
2022-07-17 10:58:14 +0500

    Advanced Combine Publishers and Subscribers

--- SwiftUI Bootcamp/Combine/AdvancedCombine.swift
+++ SwiftUI Bootcamp/Combine/AdvancedCombine.swift
@@ -9,16 +9,35 @@ import SwiftUI
 import Combine
 
 class AdvancedCombineDataService {
-        
-    @Published var basicPablisher: [String] = []
+    
+    //    @Published var basicPablisher: String = ""
+    //    let currentValuePublisher = CurrentValueSubject<Int, Error>(0)
+    let passThroughPublisher = PassthroughSubject<Int, Error>()
+    let boolPublisher = PassthroughSubject<Bool, Error>()
+    let intPublisher = PassthroughSubject<Int, Error>()
     
     init() {
         publishFakeData()
     }
     
     private func publishFakeData() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.basicPablisher = ["one", "two", "three"]
+        let items: [Int] = Array(0..<11)
+        for itemIndex in items.indices {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(itemIndex)) {
+                self.passThroughPublisher.send(items[itemIndex])
+                
+                if itemIndex > 4 && itemIndex < 8 {
+                    self.boolPublisher.send(false)
+                    self.intPublisher.send(999)
+                } else {
+                    self.boolPublisher.send(true)
+                }
+                
+                // trigger the end of sending values to subscribers, need for using .last and .last(where:) in Sequence Operations & Mathematic Operations
+                if itemIndex == items.indices.last {
+                    self.passThroughPublisher.send(completion: .finished)
+                }
+            }
         }
     }
 }
@@ -34,7 +53,95 @@ class AdvancedCombineViewModel: ObservableObject {
     }
     
     private func addSubscribers() {
-        dataService.$basicPablisher
+        dataService.passThroughPublisher
+        
+        // Sequence Operations
+//            .first()
+//            .first(where: { $0 > 4 })
+//            .tryFirst(where: { int in
+//                if int == 3 {
+//                    throw URLError(.badServerResponse)
+//                }
+//                return int > 1
+//            })
+//            .last()
+//            .last(where: { $0 < 4 })
+//            .tryFirst(where: { int in
+//                if int == 3 {
+//                    throw URLError(.badServerResponse)
+//                }
+//                return int > 1
+//            })
+//            .dropFirst()
+//            .dropFirst(3)
+//            .drop(while: { $0 < 5 })
+//            .tryDrop(while: { int in
+//                if int == 5 {
+//                    throw URLError(.badServerResponse)
+//                }
+//                return int < 6 })
+//            .prefix(4)
+//            .prefix(while: { $0 < 5 })
+//            .output(at: 7)
+//            .output(in: 2..<4)
+        
+        // Mathematic Operations
+//            .max()
+//            .max(by: { int1, int2 in
+//                return int1 < int2
+//            })
+//            .tryMax(by: )
+//            .min()
+        
+        // Filtering/Redusing
+//            .tryMap({ int in
+//                if int == 5 {
+//                    throw URLError(.badServerResponse)
+//                }
+//                return String(int)
+//            })
+//            .compactMap({ int in
+//                if int == 5 {
+//                    return nil
+//                }
+//                return String(int)
+//            })
+//            .tryCompactMap()
+//            .first(where: { $0 > 3 && $0 < 7 })
+//            .removeDuplicates()
+//            .replaceNil(with: 7)
+//            .collect()
+//            .collect(3)
+//            .allSatisfy({ $0 == 5 })
+        
+        // Timing Operations
+//            .debounce(for: 1.0, scheduler: DispatchQueue.main)
+//            .delay(for: 2, scheduler: DispatchQueue.main)
+//            .measureInterval(using: DispatchQueue.main) // time between publishers
+//            .throttle(for: 10.0, scheduler: DispatchQueue.main, latest: true) // wait between publish values
+//            .retry(3) // retry to do something if got error
+//            .timeout(0.75, scheduler: DispatchQueue.main) //if time between publishers more that 0.75 then terminating publishing
+        
+        //Multiple Publishers and Subscribers
+//            .combineLatest(dataService.boolPublisher) // listen two publishers, to start flow need values from both publishers
+//            .compactMap({ (int, bool) in
+//                if bool {
+//                    return String(int)
+//                }
+//                return nil
+//            })
+//            .compactMap({ $1 ? String($0) : nil })
+//            .merge(with: dataService.intPublisher)   // merging different publishers in to same pipeline
+//            .zip(dataService.boolPublisher)          // zipping different values in one publisher tuple
+//            .map({ tuple in
+//                return String(tuple.1) + tuple.1.description
+//            })
+//            .catch({ error in                       // return value from another publisher if error
+//                return self.dataService.intPublisher
+//            })
+        
+        
+            .map({ String($0) })
             .sink { complition in
                 switch complition {
                 case .finished:
@@ -43,10 +150,10 @@ class AdvancedCombineViewModel: ObservableObject {
                     print("Error: \(error.localizedDescription)")
                 }
             } receiveValue: { [weak self] returnedValue in
-                self?.data = returnedValue
+                self?.data.append(returnedValue)
             }
             .store(in: &cancellables)
-
+        
     }
     
 }
